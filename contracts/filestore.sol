cca73f54c7d65334db1f4171f2e030b77bc5eac2
rattle99 <24495512+rattle99@users.noreply.github.com>
2019-02-03 22:44:58 +0530

    add contract

---
+++ contracts/filestore.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.5 <0.6;
+
+contract fileStore {
+    struct userData {
+        string ipfsHash;
+        address user;
+        uint timestamp;
+    }
+    
+    mapping (address => userData) users;
+    
+    function setHash(string memory _str) public {
+        users[msg.sender].ipfsHash = _str;
+        users[msg.sender].timestamp = now;
+        
+    }
+    
+    function getHash() public view returns(string memory) {
+        return users[msg.sender].ipfsHash;
+    }
+}
