888b3bd256fa7dcfc630b6284664c116b17863f7
Sachin Saini <iamsainisachin@gmail.com>
2019-02-05 01:08:11 +0530

    add compile script

---
+++ compile.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const contractPath = path.resolve(__dirname, 'contracts', 'filestore.sol');
+const source = fs.readFileSync(contractPath, 'utf8');
+module.exports = solc.compile(source, 1).contracts[':fileStore'];
