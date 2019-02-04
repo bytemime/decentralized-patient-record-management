fc7eb31b4b4a2b97c08c3820cce1d12d50b58be2
Sachin Saini <iamsainisachin@gmail.com>
2019-02-05 01:33:34 +0530

    setup initial testing

---
+++ test/filestore.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const { interface, bytecode } = require('../compile');
+const web3 = new Web3(ganache.provider());
+let accounts;
+let filestore;
+beforeEach(async () => {
+	accounts = await web3.eth.getAccounts();
+	filestore = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: bytecode })
+		.send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('filestore', () => {
+	it('should test deployment on local network', () => {
+		assert.ok(filestore.options.address);
+	});
+});
